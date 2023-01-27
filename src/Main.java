0d904579e14b6221fb6fa02f9b086048778b98dc
Kimtai <kymdennis3@gmail.com>
2023-01-27 06:23:22 +0300

    Created another car

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         Car myCar = new Car();
+        Car myCar2 = new Car();
 
-        //System.out.println(myCar.make);
-        //System.out.println(myCar.model);
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
         //System.out.println(myCar.price);
         //System.out.println(myCar.year);
+        System.out.println();
+        System.out.println(myCar2.make);
+        System.out.println(myCar2.model);
 
         //myCar.drive();
         myCar.brake();
