259ec13675658d1ee1c89cfb8c11755a275fe189
Kimtai <kymdennis3@gmail.com>
2023-02-03 22:34:30 +0300

    Created another car

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
         //System.out.println(myCar.make);
         //System.out.println(myCar.model);
         //System.out.println(myCar.price);
-        System.out.println(myCar.year);
-        //System.out.println();
+        //System.out.println(myCar.year);
+        System.out.println();
        //System.out.println(myCar2.make);
         //System.out.println(myCar2.model);
 
